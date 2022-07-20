8cab91ba4f3621ba7c48ade5f6cc0ee5b2f37cf9
Wilkins Chung <wilkins.chung@gmail.com>
2022-07-19 17:08:23 -0700

    TEXT record signature linking rather than relying on auth[0-9A-Za-z]+.mainENS.eth

--- src/utils/LinkedAddress.ts
+++ src/utils/LinkedAddress.ts
@@ -5,29 +5,27 @@ export interface LinkedAddress {
   address: string,
 }
 
-/**
- * Get the auth linked address if it exists.  
- * If there is a linked address, returns LinkedAddress object. 
- * Otherwise returns null.
- * 
- * @param provider ethers.js provider
- * @param address  address to check
- * @returns 
- */
-export async function getLinkedAddress(provider: ethers.providers.Provider, address: string): Promise<LinkedAddress | null> {
+async function getLinkedAddress(provider: ethers.providers.EnsProvider, address: string): Promise<LinkedAddress | null> {
   const addressENS = await provider.lookupAddress(address);
   if (!addressENS) return null;
 
-  const authMatch = addressENS.match(/^(auth[0-9A-Za-z]*)\.(.*)/);
-  if (!authMatch) return null;
+  const vaultInfo = await (await provider.getResolver(addressENS))?.getText("vault");
+  if (!vaultInfo) return null;
 
-  const linkedENS = authMatch[2];
-  const linkedAddress = await provider.resolveName(linkedENS);
+  const vaultInfoArray = vaultInfo.split(':');
+  if (vaultInfoArray.length != 3 || vaultInfoArray[0] != 'eip5131') return null;
+  const authKey = vaultInfoArray[1];
+  const vaultAddress = vaultInfoArray[2];
 
-  if (!linkedAddress) return null;
+  const vaultENS = await provider.lookupAddress(vaultAddress);
+  if (!vaultENS) return null;
+
+  const expectedSigningAddress = await (await provider.getResolver(vaultENS))?.getText(authKey);
+
+  if (expectedSigningAddress != address) return null;
 
   return {
-    ens: linkedENS,
-    address: linkedAddress
+    ens: vaultENS,
+    address: vaultAddress
   };
 }
