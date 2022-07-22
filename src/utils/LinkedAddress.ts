f731e1b66b4bf929b76be6aa9b87705e7a05bfe4
jalil <jalil@1001.digital>
2022-07-22 21:10:50 +0200

    EIP prefix in TXT record keys; Throw errors where applicable

--- src/utils/LinkedAddress.ts
+++ src/utils/LinkedAddress.ts
@@ -1,28 +1,38 @@
-import { ethers } from "ethers";
+import { ethers } from 'ethers';
 
 export interface LinkedAddress {
   ens: string,
   address: string,
 }
 
-async function getLinkedAddress(provider: ethers.providers.EnsProvider, address: string): Promise<LinkedAddress | null> {
+export async function getLinkedAddress(
+  provider: ethers.providers.EnsProvider, address: string
+): Promise<LinkedAddress | null> {
   const addressENS = await provider.lookupAddress(address);
   if (!addressENS) return null;
 
-  const vaultInfo = await (await provider.getResolver(addressENS))?.getText("vault");
+  const vaultInfo = await (await provider.getResolver(addressENS))?.getText('eip5131:vault');
   if (!vaultInfo) return null;
 
   const vaultInfoArray = vaultInfo.split(':');
-  if (vaultInfoArray.length != 3 || vaultInfoArray[0] != 'eip5131') return null;
-  const authKey = vaultInfoArray[1];
-  const vaultAddress = vaultInfoArray[2];
+  if (vaultInfoArray.length !== 2) {
+    throw new Error('EIP5131: Authkey and vault address not configured correctly.');
+  }
+
+  const [ authKey, vaultAddress ] = vaultInfoArray;
 
   const vaultENS = await provider.lookupAddress(vaultAddress);
-  if (!vaultENS) return null;
+  if (!vaultENS) {
+    throw new Error(`EIP5131: No ENS domain with reverse record set for vault.`);
+  };
 
-  const expectedSigningAddress = await (await provider.getResolver(vaultENS))?.getText(authKey);
+  const expectedSigningAddress = await (
+    await provider.getResolver(vaultENS)
+  )?.getText(`eip5131:${authKey}`);
 
-  if (expectedSigningAddress != address) return null;
+  if (expectedSigningAddress?.toLowerCase() !== address.toLowerCase()) {
+    throw new Error(`EIP5131: Authentication mismatch.`);
+  };
 
   return {
     ens: vaultENS,
